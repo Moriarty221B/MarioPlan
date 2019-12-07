95bde685c66512da0c9503a827b39130553cf526
Moriarty221B <41786041+Moriarty221B@users.noreply.github.com>
2019-12-07 14:56:55 +0200

    Add files via upload
    Final commit before deployment

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -9,7 +9,7 @@ import {Redirect} from 'react-router-dom'
 class DashBoard extends Component{
     render(){
         //console.log(this.props)
-        const{projects,auth} = this.props;
+        const{projects,auth,notifications} = this.props;
         if(!auth.uid) return<Redirect to='/signin'/>
 
         return(
@@ -20,7 +20,7 @@ class DashBoard extends Component{
                         <ProjectList projects={projects}/>
                     </div>
                     <div className="col s12 m5 offset-m1">
-                        <Notifications/> 
+                        <Notifications notifications={notifications} /> 
                     </div>
                                    
                 </div>
@@ -33,10 +33,13 @@ const mapStateToProps=(state)=>{
     console.log(state);
     return{
         projects:state.firestore.ordered.projects,
-        auth:state.firebase.auth
+        auth:state.firebase.auth,
+        notifications: state.firestore.ordered.notifications
     }
 }
 /**link firebase and projects */
 export default compose ( connect(mapStateToProps),
-    firestoreConnect([{collection:'projects'}])
+    firestoreConnect([
+        {collection:'projects',orderBy:['createdAt','desc']},
+        {collection:'notifications', limit:3, orderBy:['time','desc']}])//only show 3 notifications
 )(DashBoard)
